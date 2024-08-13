8930b6ed817e5960f5fcc1e5dfb0ea3914845f12
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2024-08-13 08:47:44 +0200

    added the models for cloathing and user in frontend

---
+++ frontend/src/model/Cloathing.ts
@@ -0,0 +1,22 @@
+export type Clothing = {
+    id: string,
+    name: string,
+    type: string,
+    size: string,
+    color: string,
+    price: number,
+    brand: string,
+    material: string,
+    description: string
+}
+export type NewClothing = {
+    id: string,
+    name: string,
+    type: string,
+    size: string,
+    color: string,
+    price: number,
+    brand: string,
+    material: string,
+    description: string
+}
---
+++ frontend/src/model/User.ts
@@ -0,0 +1,9 @@
+export type User = {
+    id: string,
+    username: string,
+    password: string
+}
+export type UserModel = {
+    username: string,
+    password: string
+}
