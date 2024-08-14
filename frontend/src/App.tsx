64ada8b579352579cad9e0c9c3d526eb9b88eb42
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2024-08-14 12:00:43 +0200

    added Collection to the Project to show all CLothings in a Gallery and sort them via name type color or brand

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,19 @@ import './App.css';
 import useUser from "./customHooks/UseUser";
 import {BrowserRouter} from "react-router-dom";
 import Header from "./header/Header";
+import {useEffect} from "react";
+import useCollection from "./customHooks/UseCollection";
 
 function App() {
+    const {collection, addClothing, deleteClothing, loadCollection} = useCollection()
   const {user, login, logout, isLoading, createUser} = useUser();
 
+    useEffect(() => {
+        if (user) {
+            loadCollection();
+        }
+        //eslint-disable-next-line
+    }, []);
   function handleLogout() {
     return new Promise<void>((resolve) => {
       logout();
@@ -14,11 +23,11 @@ function App() {
     });
   }
 
-  // function handleLogin(username: string, password: string) {
-  //   return login(username, password).catch((error) => {
-  //     console.error('An error occurred during login:', error);
-  //   });
-  // }
+  function handleLogin(username: string, password: string) {
+    return login(username, password).catch((error) => {
+      console.error('An error occurred during login:', error);
+    });
+  }
   return (
       <BrowserRouter>
         <div className="App">
---
+++ frontend/src/collection.gallery/Collection.tsx
@@ -0,0 +1,60 @@
+import {Clothing} from "../model/Cloathing";
+import {useState} from "react";
+import useCollection from "../customHooks/UseCollection";
+import {MenuItem, TextField} from "@mui/material";
+import ClothingCard from "../card/ClothingCard";
+
+type CollectionProps = {
+    collection: Clothing[];
+}
+
+enum SearchOption {
+    Name = "name",
+    Type = "type",
+    Color = "color",
+    Brand = "brand"
+}
+
+export default function Collection(props: CollectionProps){
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchOption, setSearchOption] = useState(SearchOption.Name);
+    const {collection} = useCollection();
+
+    const handleSearchTermChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
+    const handleSearchOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchOption(event.target.value as SearchOption);
+    };
+    const filteredCollection = collection.filter((clothing) =>
+        clothing[searchOption].toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <div className="collection">
+            <div className="action-bar">
+                <TextField
+                    type="text"
+                    placeholder="Search for"
+                    value={searchTerm}
+                    onChange={handleSearchTermChange}
+                />
+                <TextField
+                    select
+                    value={searchOption}
+                    onChange={handleSearchOptionChange}
+                >
+                    <MenuItem value={SearchOption.Name}>Name</MenuItem>
+                    <MenuItem value={SearchOption.Type}>Type</MenuItem>
+                    <MenuItem value={SearchOption.Color}>Color</MenuItem>
+                    <MenuItem value={SearchOption.Brand}>Brand</MenuItem>
+                </TextField>
+            </div>
+            <div className="style">
+                {filteredCollection.map((card: Clothing) => (
+                    <ClothingCard clothing={card} />
+                ))}
+            </div>
+        </div>
+    );
+}
