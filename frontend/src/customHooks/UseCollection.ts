18c6c98f80626199263ad563f7e02a1cb67c9098
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2024-08-14 11:32:03 +0200

    added cusotmHook UseCollection.ts

---
+++ frontend/src/customHooks/UseCollection.ts
@@ -0,0 +1,37 @@
+import {useEffect, useState} from "react";
+import {Clothing, NewClothing} from "../model/Cloathing";
+import axios from "axios";
+import {toast} from "react-toastify";
+
+export default function useCollection() {
+    const [collection, setCollection] = useState<Clothing[]>([])
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        loadCollection()
+    }, []);
+
+    function loadCollection() {
+        axios.get("/api/collection")
+            .then((getCollection) => {
+                setCollection(getCollection.data)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+    function addClothing(newAlbum: NewClothing) {
+        axios.post("/api/collection", newAlbum)
+            .then(() => loadCollection())
+            .catch(() => console.error("post on /api/Request was not successful"))
+    }
+    function deleteClothing(id: string) {
+        axios.delete('/api/collection/' + id)
+            .then(() => {
+                setCollection(collection.filter((clothing) => clothing.id !== id))
+                toast.success("Clothing deleted successfully");
+            })
+            .catch(console.error)
+    }
+    return {collection, loadCollection, addClothing, deleteClothing, searchTerm, setSearchTerm}
+}
