4344c9ae5bff5220167c4637b61aa716f775203d
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2024-10-10 14:15:08 +0200

    adjustments in TestClasses, got rid of old integration test.

--- backend/src/test/java/com/github/steffenrothenberg/backend/clothing/controller/ClothingControllerIntegrationTest.java
+++ backend/src/test/java/com/github/steffenrothenberg/backend/clothing/controller/ClothingControllerIntegrationTest.java
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-public class ClothingControllerTest {
+public class ClothingControllerIntegrationTest {
 
     @Autowired
     private WebApplicationContext context;
